73b6dd4220097e10a6658259f7a7a5f41b6ec4b9
David Peicho <david.peicho@gmail.com>
2017-06-11 00:37:34 +0200

    structure: Update webpack config to hot reload
    - Updates the webpack config to works with hot reloading
    
    * index.html
    * package.json
    * src/index.js
    * webpack.config.js

--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ let THREE = require( 'three' );
 let scene, camera, renderer;
 let geometry, material, mesh;
 
-console.log( 'ALDLADL' );
-
 function init() {
  
     scene = new THREE.Scene();
  
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
     camera.position.z = 1000;
- 
+
     geometry = new THREE.BoxGeometry( 200, 200, 200 );
     material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
  
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 'use strict';
 
-let webpack = require('webpack');
+let path = require( 'path' );
+let webpack = require( 'webpack' );
 
 let ROOT = __dirname;
 let SRC = ROOT + '/src';
-let BUILD = ROOT + '/build';
+let BUILD = 'build';
 
 let ENTRY_FILE = SRC + '/index.js';
-let OUTPUT_FILE = BUILD + '/artflow-dist.js';
+let OUTPUT_FILE = 'artflow-dist.js';
 
 module.exports = {
  entry: ENTRY_FILE,
  output: {
-    path: BUILD,
-    filename: 'build.js'
-  }
+    path: path.resolve( __dirname, BUILD + '/' ),
+    filename: OUTPUT_FILE,
+    publicPath: '/' + BUILD + '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
