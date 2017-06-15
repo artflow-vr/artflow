892dbb350c6f9e79b5df050e5b8cd2d3492badc0
David Peicho <david.peicho@gmail.com>
2017-06-16 00:08:18 +0200

    vr-integration: Add VR controls
    - Adds vr-controls.js file from Three.js
    
    * .eslintignore
    * index.html
    * package.json
    * src/artflow.js
    * src/controls/controls.js
    * src/controls/vr-controls.js
    * src/index.js
    * src/vr/vr-effect.js
    * src/vr/vr.js
    * src/vr/webvr.js

--- src/artflow.js
+++ src/artflow.js
@@ -1,8 +1,10 @@
 'use strict';
 
 let vrNamespace = require( './vr/vr' );
+let controlsNamespace = require( './controls/controls' );
 
 let Artflow = {};
 Artflow.vr = vrNamespace;
+Artflow.controls = controlsNamespace;
 
 module.exports = Artflow;
--- src/vr/vr.js
+++ src/vr/vr.js
@@ -1,6 +1,8 @@
 let WebVR = require( './webvr' );
+let VREffect = require( './vr-effect' );
 
 let vr = {};
 vr.WebVR = WebVR;
+vr.VREffect = VREffect;
 
 module.exports = vr;
