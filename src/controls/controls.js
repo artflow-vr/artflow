e07a01553adc3b07ee0cbb3a04aa406498fb3da7
David Peicho <david.peicho@epita.fr>
2017-08-06 00:34:58 +0200

    vr-integration: Add Newton parabola equation
    - Telporter uses Newton second law to select
    jump location
    - Displays sample spline for teleportation position

--- src/controls/controls.js
+++ src/controls/controls.js
@@ -1,8 +1,10 @@
 let VRControls = require( './vr-controls' );
 let FPSControls = require( './fps-controls' );
+let TeleporterController = require( './teleporter-control' );
 
 let controls = {};
 controls.VRControls = VRControls;
 controls.FPSControls = FPSControls;
+controls.TeleporterController = TeleporterController;
 
 module.exports = controls;
