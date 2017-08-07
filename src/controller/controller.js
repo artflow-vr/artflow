77f754340b24638d5039bd955ba91485deeefbc8
David Peicho <david.peicho@epita.fr>
2017-08-07 14:23:35 +0200

    vr-integration: Add teleportation in VR and Non-VR

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,8 +1,6 @@
 let FPSControls = require( './fps-controller' );
 let TeleporterController = require( './teleporter-controller' );
 
-require( './vive-controller' );
-
 let controller = {};
 controller.FPSControls = FPSControls;
 controller.TeleporterController = TeleporterController;
