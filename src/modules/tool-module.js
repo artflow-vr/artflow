dcffb229c90912ebffc0eeb8efa99bb591823edc
David Peicho <david.peicho@epita.fr>
2017-08-15 15:27:20 +0200

    tool_module: brush: move brush controller to tool
    - Adds tool registering
    - Move brush from feature/core/brush to a tool

--- src/modules/tool-module.js
+++ src/modules/tool-module.js
@@ -1,11 +1,16 @@
 'use strict';
 
+let Tool = require( './tool/tool' );
+
+let MainView = require( '../view/main-view' );
+
 let Utils = require( '../utils/utils' );
 let EventDispatcher = Utils.EventDispatcher;
 
 let ToolModule = module.exports;
 
 // Contains every tool: brush, water, etc...
+// These tools are not instanciated, they represent only a blueprint.
 let _tools = {};
 
 // Contains the tool for each controller. Whenever the user is not in VR,
@@ -27,27 +32,51 @@ ToolModule.register = function ( toolID, tool ) {
 
 ToolModule.init = function () {
 
-    _selected[ 0 ] = null;
+    this._registerBasicTools();
+
+    // TODO: We have to instanciate the tools according to what the user
+    // selected. We should keep track of instanciated tool, to avoid
+    // making useless instanciation.
+    _selected[ 0 ] = new _tools.Brush();
     _selected[ 1 ] = null;
 
-    EventDispatcher.register( 'interactDown', function () {} );
+    MainView.addToMovingGroup( _selected[ 0 ].view.getObject() );
 
-    EventDispatcher.register( 'interactUp', function () {} );
+    EventDispatcher.register( 'interactDown', function ( data ) {
 
-    EventDispatcher.register( 'undo', function () {
-        // TODO: Handles undo by keeping operations and their order.
+        if ( _selected[ data.controllerID ].triggerChild )
+            _selected[ data.controllerID ].triggerChild( data );
+
     } );
 
-    EventDispatcher.register( 'redo', function () {
-        // TODO: Handles redo by keeping operations and their order.
+    EventDispatcher.register( 'interactUp', function ( data ) {
+
+        if ( _selected[ data.controllerID ].releaseChild )
+            _selected[ data.controllerID ].releaseChild( data );
+
+    } );
+
+    EventDispatcher.register( 'interact', function ( data ) {
+
+        if ( _selected[ data.controllerID ].useChild )
+            _selected[ data.controllerID ].useChild( data );
+
     } );
 
 };
 
 ToolModule.update = function ( delta ) {
 
-    if ( _selected[ 0 ] ) _selected[ 0 ].update( delta );
+    if ( _selected[ 0 ] && _selected[ 0 ].updateChild )
+        _selected[ 0 ].updateChild( delta );
 
-    if ( _selected[ 1 ] ) _selected[ 1 ].update( delta );
+    if ( _selected[ 1 ] && _selected[ 1 ].updateChild )
+        _selected[ 1 ].updateChild( delta );
+
+};
+
+ToolModule._registerBasicTools = function () {
+
+    this.register( 'Brush', Tool.BrushTool );
 
 };
