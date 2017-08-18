f9819b40aef5911210edbe7600ec82fd58a96987
Chady Dimachkie <cpcdoy@gmail.com>
2017-08-18 19:16:31 +0200

    Add: brush ending + Fix: motion shaking + Clean: refactor brush-tool.js

--- src/modules/tool/brush-tool.js
+++ src/modules/tool/brush-tool.js
@@ -16,16 +16,20 @@ function BrushTool( options ) {
 
     this._verticesCount = 0;
     this._normalsCount = 0;
+    this._uvCount = 0;
     this._vboLimit = 10000;
     this._maxSpread = 10;
+    this._maxSpreadAdjust = 0;
     this._material = null;
     this._geometry = null;
     this._vertices = null;
     this._normals = null;
     this._uvs = null;
+    this._delta = 0.05;
     this._axisLock = new THREE.Vector3( 0, 0, -1 );
     this._pointA = new THREE.Vector3( 0, 0, 0 );
     this._pointB = new THREE.Vector3( 0, 0, 0 );
+    this._lastPoint = new THREE.Vector3( -99999, -9999, -99999 );
 
     if ( this.options.texture ) {
 
@@ -71,6 +75,13 @@ BrushTool.prototype.use = function ( data ) {
 
 };
 
+BrushTool.prototype.release = function ( data ) {
+
+
+    this._addLastPoint( data.position.world, data.orientation );
+
+};
+
 BrushTool.prototype.trigger = function () {
 
     this._geometry = new THREE.BufferGeometry();
@@ -88,6 +99,8 @@ BrushTool.prototype.trigger = function () {
 
     this._verticesCount = 0;
     this._normalsCount = 0;
+    this._uvCount = 0;
+    this._maxSpreadAdjust = 0;
 
     let mesh = new THREE.Mesh( this._geometry, this._material );
     mesh.drawMode = THREE.TriangleStripDrawMode;
@@ -105,18 +118,10 @@ BrushTool.prototype.trigger = function () {
 
 };
 
-BrushTool.prototype._addPoint = function ( pointCoords, orientation ) {
+BrushTool.prototype._processPoint = function ( pointCoords, orientation, verticesCount_, normalsCount_ ) {
 
-    let uvCount = 0;
-    for ( let i = 0; i < this._verticesCount / 2; i++ ) {
-        let iMod = i % this._maxSpread;
-
-        this._uvs[ uvCount++ ] = iMod / ( this._maxSpread - 1 );
-        this._uvs[ uvCount++ ] = 0;
-
-        this._uvs[ uvCount++ ] = iMod / ( this._maxSpread - 1 );
-        this._uvs[ uvCount++ ] = 1;
-    }
+    let verticesCount = verticesCount_;
+    let normalsCount = normalsCount_;
 
     this._axisLock.x = 1.0;
     this._axisLock.y = 0.0;
@@ -129,25 +134,26 @@ BrushTool.prototype._addPoint = function ( pointCoords, orientation ) {
     this._pointA.z = pointCoords.z;
     this._pointA.sub( this._axisLock );
 
-    this._vertices[ this._verticesCount++ ] = this._pointA.x;
-    this._vertices[ this._verticesCount++ ] = this._pointA.y;
-    this._vertices[ this._verticesCount++ ] = this._pointA.z;
+    this._vertices[ verticesCount++ ] = this._pointA.x;
+    this._vertices[ verticesCount++ ] = this._pointA.y;
+    this._vertices[ verticesCount++ ] = this._pointA.z;
 
     this._pointB.x = pointCoords.x;
     this._pointB.y = pointCoords.y;
     this._pointB.z = pointCoords.z;
     this._pointB.add( this._axisLock );
-    this._vertices[ this._verticesCount++ ] = this._pointB.x;
-    this._vertices[ this._verticesCount++ ] = this._pointB.y;
-    this._vertices[ this._verticesCount++ ] = this._pointB.z;
+
+    this._vertices[ verticesCount++ ] = this._pointB.x;
+    this._vertices[ verticesCount++ ] = this._pointB.y;
+    this._vertices[ verticesCount++ ] = this._pointB.z;
 
     if ( this._verticesCount >= 3 * 4 ) {
-        let v0 = new THREE.Vector3( this._vertices[ this._verticesCount - 9 -
-                3 ], this._vertices[ this._verticesCount - 9 - 2 ],
-            this._vertices[ this._verticesCount - 9 - 1 ] );
-        let v1 = new THREE.Vector3( this._vertices[ this._verticesCount - 6 -
-                3 ], this._vertices[ this._verticesCount - 6 - 2 ],
-            this._vertices[ this._verticesCount - 6 - 1 ] );
+        let v0 = new THREE.Vector3( this._vertices[ verticesCount - 9 -
+            3 ], this._vertices[ verticesCount - 9 - 2 ],
+            this._vertices[ verticesCount - 9 - 1 ] );
+        let v1 = new THREE.Vector3( this._vertices[ verticesCount - 6 -
+            3 ], this._vertices[ verticesCount - 6 - 2 ],
+            this._vertices[ verticesCount - 6 - 1 ] );
         let v2 = this._pointA;
 
         let v0Subv1 = v0.sub( v1 );
@@ -156,22 +162,77 @@ BrushTool.prototype._addPoint = function ( pointCoords, orientation ) {
         let n1 = v0Subv1;
         n1.cross( v2Subv1 );
 
-        this._normals[ this._normalsCount++ ] = n1.x;
-        this._normals[ this._normalsCount++ ] = n1.y;
-        this._normals[ this._normalsCount++ ] = n1.z;
+        this._normals[ normalsCount++ ] = n1.x;
+        this._normals[ normalsCount++ ] = n1.y;
+        this._normals[ normalsCount++ ] = n1.z;
 
-        this._normals[ this._normalsCount++ ] = n1.x;
-        this._normals[ this._normalsCount++ ] = n1.y;
-        this._normals[ this._normalsCount++ ] = n1.z;
+        this._normals[ normalsCount++ ] = n1.x;
+        this._normals[ normalsCount++ ] = n1.y;
+        this._normals[ normalsCount++ ] = n1.z;
 
         this._geometry.normalizeNormals();
     }
+
+};
+
+BrushTool.prototype._addLastPoint = function ( pointCoords, orientation ) {
+
+    for ( let uv = this._uvCount - 8; uv < this._uvCount + 4; uv ) {
+
+        let iMod = uv;
+
+        this._uvs[ uv++ ] = iMod / ( this._uvCount - 1 );
+        this._uvs[ uv++ ] = 0;
+
+        this._uvs[ uv++ ] = iMod / ( this._uvCount - 1 );
+        this._uvs[ uv++ ] = 1;
+
+    }
+
+    let point = pointCoords.clone();
+    this._processPoint( point, orientation, this._verticesCount, this._normalsCount );
+
+    this._verticesCount += 6;
+    this._normalsCount += 6;
 
     this._geometry.attributes.normal.needsUpdate = true;
     this._geometry.attributes.position.needsUpdate = true;
     this._geometry.attributes.uv.needsUpdate = true;
 
     this._geometry.setDrawRange( 0, this._verticesCount / 3 );
+
+    this._lastPoint = pointCoords.clone();
+
+};
+
+BrushTool.prototype._addPoint = function ( pointCoords, orientation ) {
+
+    if ( this._lastPoint.distanceTo( pointCoords ) < this._delta )
+        return;
+
+    let iMod = ( this._uvCount / 4 ) % ( this._maxSpread + this._maxSpreadAdjust );
+
+    if ( this._uvCount / 4 > this._maxSpread / 2 )
+        this._maxSpreadAdjust += 2;
+
+    this._uvs[ this._uvCount++ ] = iMod / ( this._maxSpread + this._maxSpreadAdjust - 1 );
+    this._uvs[ this._uvCount++ ] = 0;
+
+    this._uvs[ this._uvCount++ ] = iMod / ( this._maxSpread + this._maxSpreadAdjust - 1 );
+    this._uvs[ this._uvCount++ ] = 1;
+
+    this._processPoint( pointCoords.clone(), orientation, this._verticesCount, this._normalsCount );
+
+    this._verticesCount += 6;
+    this._normalsCount += 6;
+
+    this._geometry.attributes.normal.needsUpdate = true;
+    this._geometry.attributes.position.needsUpdate = true;
+    this._geometry.attributes.uv.needsUpdate = true;
+
+    this._geometry.setDrawRange( 0, this._verticesCount / 3 );
+
+    this._lastPoint = pointCoords.clone();
 };
 
 module.exports = BrushTool;
