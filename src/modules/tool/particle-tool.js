4aa0a3c301b779e1178d8e712a747c253c184d80
AurelienLeandri <aurelien.leandri@gmail.com>
2017-08-20 21:44:35 +0200

    Add particle tool as brush

--- src/modules/tool/particle-tool.js
+++ src/modules/tool/particle-tool.js
@@ -6,15 +6,42 @@ function ParticleTool( options ) {
 
     AbstractTool.call( this, options );
     this.setOptionsIfUndef( {
-        speed: 100
+        brush_size: 20,
+        particles_thickness: 0.2
     } );
 
+    // this._attribute = 0; TODO: attributes go there
+    this._position = THREE.Vector3();
 }
+
 ParticleTool.prototype = Object.create( AbstractTool.prototype );
 ParticleTool.prototype.constructor = ParticleTool;
 
-ParticleTool.prototype.update = function () { };
+BrushTool.prototype.use = function ( data ) {
 
-ParticleTool.prototype.trigger = function () { };
+    this._update_brush( data.position.world );
+
+};
+
+BrushTool.prototype._update_brush = function ( pointCoords ) {
+    this._position.x = pointCoords.x;
+    this._position.y = pointCoords.y;
+    this._position.z = pointCoords.z;
+    this._pointer_mesh.radius = this.options.brush_size;
+};
+
+ParticleTool.prototype.update = function () {};
+
+ParticleTool.prototype.trigger = function () {
+
+    let mesh = new THREE.Sphere( this.radius, this._material );
+
+    mesh.castShadow = false;
+    mesh.receiveShadow = false;
+
+    this.view.addTHREEObject( mesh );
+
+    return new AddCommand( this.view, mesh );
+};
 
 ParticleTool.prototype.release = function () { };
