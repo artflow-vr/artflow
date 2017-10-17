83f0ad2b35c0627224029a14573bd6d52bfc1b2a
Sarasvati Moutoucomarapoulé <moutou_s@epita.fr>
2017-10-17 20:32:34 +0200

    tree: tree structure to enable multiple growing trees
    * src/modules/tool/tree-tool.js: Here.

--- src/modules/tool/tree-tool.js
+++ src/modules/tool/tree-tool.js
@@ -32,16 +32,59 @@ import { LSystem } from '../../utils/l-system';
 
 class State {
 
-  constructor( pos, angle, step , orientation, pressure ) {
+    constructor( pos, angle, step , orientation, pressure ) {
 
-    this.pos = pos;
-    this.angle = angle;
-    this.step = step;
-    this.orientation = orientation;
-    this.pressure = pressure;
+        this.pos = pos;
+        this.angle = angle;
+        this.step = step;
+        this.orientation = orientation;
+        this.pressure = pressure;
 
-  }
+    }
 
+}
+
+class Tree {
+
+    constructor ( options ) {
+
+        this.states = [];
+        this.helper = new BrushHelper( options );
+
+    }
+
+    init( data, angle, step, str ) {
+
+        this.pushState(
+            data.position.world, Math.PI / 2, step, data.orientation,
+            data.pressure
+        );
+        this.angle = angle;
+        this.step = step;
+        this.str = str;
+
+    }
+
+    pushState( pos, angle, step, orientation, pressure ) {
+
+        let state = new State(
+            pos.clone(), angle, step, orientation.clone(), pressure
+        );
+        this.states.push( state );
+
+    }
+
+    popState() {
+
+        this.states.pop();
+
+    }
+
+    peekState() {
+
+        return this.states[ this.states.length - 1 ];
+
+    }
 }
 
 export default class TreeTool extends AbstractTool {
@@ -60,24 +103,27 @@ export default class TreeTool extends AbstractTool {
             materialId: 'material_without_tex'
         } );
 
-        this._helper = new BrushHelper( options );
-
         this.registerEvent( 'interact', {
             trigger: this.trigger.bind( this ),
             release: this.release.bind( this )
         } );
 
-        this.mesh = null;
 
         //this._lSystem = new LSystem( '--F[+F][-F[-F]F]F[+F][-F]', '' );
         //this._lSystem = new LSystem( 'F-F-F-F-F', 'F->F-F+F+FF-F-F+F' );
-        this._lSystem = new LSystem( 'X', `X->F[+X][-X]FX
-                                           F->FF` );
+
+        this.axiom = 'X';
+
+        this.grammar = `X->F[+X][-X]FX
+                        F->FF`;
+
+        this._lSystem = new LSystem( this.axiom, this.grammar );
+
         this._str = this._lSystem.derivate( 5 );
 
-        this._step = 0.25;
+        this.step = 0.25;
 
-        this._angle = ( 25.7 / 360 ) * 2 * Math.PI;
+        this.angle = ( 25.7 / 360 ) * 2 * Math.PI;
 
         this.interpretations = {
             'F': this.drawForward.bind( this ),
@@ -88,105 +134,136 @@ export default class TreeTool extends AbstractTool {
             ']': this.popState.bind( this )
         };
 
-        this.states = [];
+        this.trees = [];
     }
 
     trigger() {
 
-        this.mesh = this._helper.createMesh();
-        this.worldGroup.addTHREEObject( this.mesh );
-        return new AddCommand( this.worldGroup, this.mesh );
+        let tree = new Tree( this.options );
+        this.trees.push( tree );
+        this._addMesh( tree );
 
     }
 
     release( data ) {
 
-        let state = new State(
-          data.position.world.clone(), Math.PI / 2, this._step,
-          data.orientation, data.pressure
-        );
-        this.states.push( state );
-        this._draw( state );
-        this._interpret();
-        this.states.pop();
+        let tree = this.trees[ this.trees.length - 1 ];
+        tree.init( data, this.angle, this.step, this._str );
+        this._draw( tree );
+        this._interpretSbs( tree, 0 );
 
     }
 
-    _interpret() {
+    _addMesh( tree ) {
 
-        for ( let i = 0; i < this._str.length; ++i ) {
-            let newMesh = i + 1 < this._str.length
-                          && this._str[ i + 1 ].symbol !== ']'
-                          && this._str[ i + 1 ].symbol !== '[';
-            let clbk = this.interpretations[ this._str[ i ].symbol ];
-            if ( clbk ) {
-                clbk( newMesh );
-            }
+        let mesh = tree.helper.createMesh();
+        this.worldGroup.addTHREEObject( mesh );
+        return new AddCommand( this.worldGroup, mesh );
+
+    }
+
+    //_interpret( tree ) {
+
+        //for ( let i = 0; i < tree.str.length; ++i ) {
+            //let newMesh = i + 1 < tree.str.length
+                          //&& tree.str[ i + 1 ].symbol !== ']'
+                          //&& tree.str[ i + 1 ].symbol !== '[';
+            //let clbk = this.interpretations[ tree.str[ i ].symbol ];
+            //if ( clbk ) {
+                //clbk( tree, newMesh );
+            //}
+        //}
+
+        //this.trees.pop();
+
+    //}
+
+    _interpretSbs( tree, i ) {
+
+        if ( i >= tree.str.length ) {
+            this.trees.slice( this.trees.indexOf( tree ) );
+            return;
         }
 
+        let newMesh = i + 1 < tree.str.length
+                      && tree.str[ i + 1 ].symbol !== ']'
+                      && tree.str[ i + 1 ].symbol !== '[';
+        let clbk = this.interpretations[ tree.str[ i ].symbol ];
+
+        if ( clbk ) {
+            clbk( tree, newMesh );
+        }
+
+        setTimeout( this._interpretSbs.bind( this, tree, i + 1 ), 3 );
+
     }
 
-    _movePos( state ) {
+    _movePos( tree ) {
 
+        let state = tree.peekState();
         state.pos.x += state.step * Math.cos( state.angle );
         state.pos.y += state.step * Math.sin( state.angle );
 
     }
 
-    _draw( state ) {
+    _draw( tree ) {
 
-        this._helper.addPoint(
+        let state = tree.peekState();
+        tree.helper.addPoint(
             state.pos, state.orientation, state.pressure
         );
-}
-
-    drawForward() {
-        let state = this.states[ this.states.length - 1 ];
-        this._movePos( state );
-        this._draw( state );
-    }
-
-    moveForward() {
-
-        this.trigger();
-        this._movePos( this.states[ this.states.length - 1 ] );
 
     }
 
-    turnLeft() {
+    drawForward( tree ) {
 
-        this.states[ this.states.length - 1 ].angle -= this._angle;
+        this._movePos( tree );
+        this._draw( tree );
 
     }
 
-    turnRight() {
+    moveForward( tree ) {
 
-        this.states[ this.states.length - 1 ].angle += this._angle;
+        this._addMesh( tree );
+        this._movePos( tree );
+
     }
 
-    pushState( newMesh ) {
+    turnLeft( tree ) {
 
-        let state = this.states[ this.states.length - 1 ];
-        let newState = new State(
-            state.pos.clone(), state.angle, state.step, state.orientation,
+        tree.peekState().angle -= tree.angle;
+
+    }
+
+    turnRight( tree ) {
+
+       tree.peekState().angle += tree.angle;
+    }
+
+    pushState( tree, newMesh ) {
+
+        let state = tree.peekState();
+        tree.pushState(
+            state.pos, state.angle, state.step, state.orientation,
             state.pressure
-          );
-        this.states.push( newState );
+        );
 
         if ( newMesh ) {
-            this.trigger();
-            this._draw( newState );
+            this._addMesh( tree );
+            this._draw( tree );
         }
+
     }
 
-    popState( newMesh ) {
+    popState( tree, newMesh ) {
 
-        this.states.pop();
+        tree.popState();
 
         if ( newMesh ) {
-            this.trigger();
-            this._draw( this.states[ this.states.length - 1 ] );
+            this._addMesh( tree );
+            this._draw( tree );
         }
+
     }
 }
 
